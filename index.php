f6cee36619f5e837cf2371f029c932bcc12eb41a
Longinus <jesuscaesar@outlook.com>
2022-01-10 22:28:29 +1000

    Latest update

--- index.php
+++ index.php
@@ -367,7 +367,6 @@ if ($mode == 'app') {
 </div>
 <div class='bottom'>
 <img class="hover" style="height:84%;position:relative;" src="sys.start.png?rev=<?=time();?>" onclick="window.location.href = 'index.php?mode=menu';">
-<img class="hover" style="height:84%;position:relative;" src="sys.files.png?rev=<?=time();?>" onclick="window.location.href = 'index.php?mode=glob';">
 <img class="hover" style="height:84%;position:relative;" src="sys.back.png?rev=<?=time();?>" onclick="window.location.href = 'index.php?mode=view';">
 <img class="hover" style="height:84%;position:relative;" src="sys.settings.png?rev=<?=time();?>" onclick="window.location.href = 'index.php?mode=set';">
 <img class="hover" style="height:84%;position:relative;" src="sys.upd.png?rev=<?=time();?>" onclick="get('i','from','windos','flossely');">
