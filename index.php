aa456b5ea35e7acc743975ffa8cfdf82af7c76a9
Longinus <jesuscaesar@outlook.com>
2022-04-20 11:48:35 +0000

    Add

--- index.php
+++ index.php
@@ -171,11 +171,7 @@ input, select, textarea {
     position: relative;
 }
 </style>
-<script src="jquery.js"></script>
-<script src="base.js"></script>
-<script src="file.js"></script>
-<script src="edit.js"></script>
-<script src="http://www.midijs.net/lib/midi.js"></script>
+<?php include 'include.php'; ?>
 <script>
 window.onload = function() {
 <?php if ($mode == 'glob') { ?>
@@ -191,7 +187,7 @@ window.onload = function() {
 <body>
 <div class='top'>
 <p align='center'>
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:70%;position:relative;" src="logo.png?rev=<?=time();?>" onclick="window.location.href='index.php';">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:70%;position:relative;" src="logo.png?rev=<?=time();?>" onclick="window.location.href='index.php';">
 </p>
 </div>
 <div class='panel'>
@@ -204,7 +200,7 @@ if ($mode == 'app') {
         $appIcon = (file_exists($appExp[1])) ? $appExp[1] : 'sys.app.png';
         $appAction = $appExp[2];
 ?>
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:18%;position:relative;" src="<?=$appIcon;?>?rev=<?=time();?>" title="<?=$appTitle;?>" onclick="<?=$appAction;?>">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:18%;position:relative;" src="<?=$appIcon;?>?rev=<?=time();?>" title="<?=$appTitle;?>" onclick="<?=$appAction;?>">
 <?php
 }} elseif ($mode == 'pkg') {
     foreach ($list as $key=>$value) {
@@ -221,7 +217,7 @@ if ($mode == 'app') {
         $pkgCreated = $pkgHeadExp[4];
         $pkgDescription = $pkgHeadExp[5];
 ?>
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:18%;position:relative;" src="sys.pkg.png?rev=<?=time();?>" name="<?=$pkgName;?>" title="<?=$pkgName;?>" onclick="get('d', '', this.name, 'from', '', 'here');">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:18%;position:relative;" src="sys.pkg.png?rev=<?=time();?>" name="<?=$pkgName;?>" title="<?=$pkgName;?>" onclick="get('d', '', this.name, 'from', '', 'here');">
 <?php
 }} elseif ($mode == 'glob') {
     foreach ($list as $key=>$value) {
@@ -279,7 +275,7 @@ if ($mode == 'app') {
             }
         }
 ?>
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:18%;position:relative;" src="<?=$icon;?>?rev=<?=time();?>" name="<?=$value;?>" title="<?=$value;?>" onclick="<?=$link;?>">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:18%;position:relative;" src="<?=$icon;?>?rev=<?=time();?>" name="<?=$value;?>" title="<?=$value;?>" onclick="<?=$link;?>">
 <?php }} elseif ($mode == 'get') { ?>
 <p align='center'>Execute GET sequence command:<br><input type='text' style="width:45%;position:relative;" value='' onkeydown="if (event.keyCode == 13) {
     eval(this.value);
@@ -290,17 +286,17 @@ if ($mode == 'app') {
 <video style="width:100%;height:100%;" id="video" src="<?=$name;?>" controls autoplay="yes">
 <?php } elseif ($mode == 'set') { ?>
 <p align="center">
-<input type='button' id="muteSound" onmouseover="playAudio(soundPlayer, 'default.flac?rev=<?=time();?>');" name="<?=$sounds;?>" value="<?php if ($sounds) { ?>Mute<?php } else { ?>Unmute<?php } ?>" onclick="mute(muteSound.name);">
+<input type='button' id="muteSound" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" name="<?=$sounds;?>" value="<?php if ($sounds) { ?>Mute<?php } else { ?>Unmute<?php } ?>" onclick="mute(muteSound.name);">
 </p>
 <?php } elseif ($mode == 'edit') { ?>
-<img class="actionIcon" onmouseover="playAudio(soundPlayer, 'default.flac');" src="sys.cl.png?rev=<?=time();?>" id="newButton" onclick="var name = 'file'; window.location.href='index.php?mode=edit&name='+name;">
-<img class="actionIcon" onmouseover="playAudio(soundPlayer, 'default.flac');" src="sys.rd.png?rev=<?=time();?>" id="openButton" onclick="var name = filename.value; window.location.href = 'index.php?mode=edit&name=' + name + '&lock=false';">
-<img class="actionIcon" onmouseover="playAudio(soundPlayer, 'default.flac');" src="sys.wr.png?rev=<?=time();?>" id="saveButton" onclick="save();">
-<img class="actionIcon" onmouseover="playAudio(soundPlayer, 'default.flac');" src="sys.md.png?rev=<?=time();?>" id="mkdirButton" onclick="var name = filename.value; mkdir(name, false);">
-<img class="actionIcon" onmouseover="playAudio(soundPlayer, 'default.flac');" src="sys.mv.png?rev=<?=time();?>" id="moveButton" onclick="var name = filename.value; var to = doto.value; move(name, to, false);">
-<img class="actionIcon" onmouseover="playAudio(soundPlayer, 'default.flac');" src="sys.cp.png?rev=<?=time();?>" id="copyButton" onclick="var name = filename.value; var to = doto.value; copy(name, to, false);">
-<img class="actionIcon" onmouseover="playAudio(soundPlayer, 'default.flac');" src="sys.rm.png?rev=<?=time();?>" id="deleteButton" onclick="var name = filename.value; del(name, false);">
-<img class="actionIcon" onmouseover="playAudio(soundPlayer, 'default.flac');" src="sys.home.png?rev=<?=time();?>" id="homeButton" onclick="window.location.href = 'index.php';"><br>
+<img class="actionIcon" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" src="sys.cl.png?rev=<?=time();?>" id="newButton" onclick="var name = 'file'; window.location.href='index.php?mode=edit&name='+name;">
+<img class="actionIcon" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" src="sys.rd.png?rev=<?=time();?>" id="openButton" onclick="var name = filename.value; window.location.href = 'index.php?mode=edit&name=' + name + '&lock=false';">
+<img class="actionIcon" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" src="sys.wr.png?rev=<?=time();?>" id="saveButton" onclick="save();">
+<img class="actionIcon" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" src="sys.md.png?rev=<?=time();?>" id="mkdirButton" onclick="var name = filename.value; mkdir(name, false);">
+<img class="actionIcon" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" src="sys.mv.png?rev=<?=time();?>" id="moveButton" onclick="var name = filename.value; var to = doto.value; move(name, to, false);">
+<img class="actionIcon" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" src="sys.cp.png?rev=<?=time();?>" id="copyButton" onclick="var name = filename.value; var to = doto.value; copy(name, to, false);">
+<img class="actionIcon" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" src="sys.rm.png?rev=<?=time();?>" id="deleteButton" onclick="var name = filename.value; del(name, false);">
+<img class="actionIcon" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" src="sys.home.png?rev=<?=time();?>" id="homeButton" onclick="window.location.href = 'index.php';"><br>
 <label>Filename: </label>
 <input class="text" size=30 id="filename" style="width:38%;" type="text" value="<?=$editname;?>">
 <input class="text" size=30 id="doto" style="width:38%;" type="text" value="">
@@ -308,30 +304,30 @@ if ($mode == 'app') {
 <input class="text" size=30 id="findbox" style="width:36%;" type="text" value="">
 <label> to </label>
 <input class="text" size=30 id="replacebox" style="width:36%;" type="text" value="">
-<input type="button" onmouseover="playAudio(soundPlayer, 'default.flac');" class="actionButton" value=">" id="replaceButton" onclick="replaceText(findbox.value); countText();">
+<input type="button" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" class="actionButton" value=">" id="replaceButton" onclick="replaceText(findbox.value); countText();">
 <br>
 <label id="statusBar" style="width:98%;"></label>
 <?php } elseif ($mode == 'menu') { ?>
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:22%;position:relative;" src="sys.back.png?rev=<?=time();?>" title="Background Preview" onclick="window.location.href = 'index.php?mode=view';">
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:22%;position:relative;" src="sys.files.png?rev=<?=time();?>" title="File Explorer" onclick="window.location.href = 'index.php?mode=glob';">
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:22%;position:relative;" src="sys.fon.png?rev=<?=time();?>" title="Fonts" onclick="window.location.href = 'index.php?mode=glob&q=.ttf,.otf,.ttc';">
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:22%;position:relative;" src="sys.setup.png?rev=<?=time();?>" title="Install Software" onclick="window.location.href = 'index.php?mode=get';">
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:22%;position:relative;"  src="sys.apps.png?rev=<?=time();?>" title="Installed Programs" onclick="window.location.href = 'index.php?mode=app';">
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:22%;position:relative;" src="sys.txt.png?rev=<?=time();?>" title="My Documents" onclick="window.location.href = 'index.php?mode=glob&q=.txt,.pdf,.odt,.doc';">
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:22%;position:relative;" src="sys.aud.png?rev=<?=time();?>" title="My Music" onclick="window.location.href = 'index.php?mode=glob&q=.mp3,.aac,.flac,.mid';">
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:22%;position:relative;" src="sys.img.png?rev=<?=time();?>" title="My Pictures" onclick="window.location.href = 'index.php?mode=glob&q=.png,.gif,.svg,.jpg';">
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:22%;position:relative;" src="sys.vid.png?rev=<?=time();?>" title="My Videos" onclick="window.location.href = 'index.php?mode=glob&q=.mp4,.webm,.mkv';">
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:22%;position:relative;" src="sys.edit.png?rev=<?=time();?>" title="Notepad" onclick="window.location.href = 'index.php?mode=edit&name=file&lock=true';">
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:22%;position:relative;" src="sys.pkg.png?rev=<?=time();?>" title="Remove Software" onclick="window.location.href = 'index.php?mode=pkg';">
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:22%;position:relative;" src="sys.settings.png?rev=<?=time();?>" title="System Settings" onclick="window.location.href = 'index.php?mode=set';">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:22%;position:relative;" src="sys.back.png?rev=<?=time();?>" title="Background Preview" onclick="window.location.href = 'index.php?mode=view';">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:22%;position:relative;" src="sys.files.png?rev=<?=time();?>" title="File Explorer" onclick="window.location.href = 'index.php?mode=glob';">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:22%;position:relative;" src="sys.fon.png?rev=<?=time();?>" title="Fonts" onclick="window.location.href = 'index.php?mode=glob&q=.ttf,.otf,.ttc';">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:22%;position:relative;" src="sys.setup.png?rev=<?=time();?>" title="Install Software" onclick="window.location.href = 'index.php?mode=get';">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:22%;position:relative;"  src="sys.apps.png?rev=<?=time();?>" title="Installed Programs" onclick="window.location.href = 'index.php?mode=app';">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:22%;position:relative;" src="sys.txt.png?rev=<?=time();?>" title="My Documents" onclick="window.location.href = 'index.php?mode=glob&q=.txt,.pdf,.odt,.doc';">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:22%;position:relative;" src="sys.aud.png?rev=<?=time();?>" title="My Music" onclick="window.location.href = 'index.php?mode=glob&q=.mp3,.aac,.flac,.mid';">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:22%;position:relative;" src="sys.img.png?rev=<?=time();?>" title="My Pictures" onclick="window.location.href = 'index.php?mode=glob&q=.png,.gif,.svg,.jpg';">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:22%;position:relative;" src="sys.vid.png?rev=<?=time();?>" title="My Videos" onclick="window.location.href = 'index.php?mode=glob&q=.mp4,.webm,.mkv';">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:22%;position:relative;" src="sys.edit.png?rev=<?=time();?>" title="Notepad" onclick="window.location.href = 'index.php?mode=edit&name=file&lock=true';">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:22%;position:relative;" src="sys.pkg.png?rev=<?=time();?>" title="Remove Software" onclick="window.location.href = 'index.php?mode=pkg';">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:22%;position:relative;" src="sys.settings.png?rev=<?=time();?>" title="System Settings" onclick="window.location.href = 'index.php?mode=set';">
 <?php } ?>
 </div>
 <div class='bottom'>
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:84%;position:relative;" src="sys.start.png?rev=<?=time();?>" onclick="window.location.href = 'index.php?mode=menu';">
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:84%;position:relative;" src="sys.back.png?rev=<?=time();?>" onclick="window.location.href = 'index.php?mode=view';">
-<img class="hover" onmouseover="playAudio(soundPlayer, 'default.flac');" style="height:84%;position:relative;" src="sys.settings.png?rev=<?=time();?>" onclick="window.location.href = 'index.php?mode=set';">
-<img class="hover" style="height:84%;position:relative;" src="sys.upd.png?rev=<?=time();?>" onmouseover="playAudio(soundPlayer, 'default.flac');" onclick="get('i','','from','windos','','flossely');">
-<img class="hover" style="height:84%;position:relative;" src="sys.exit.png?rev=<?=time();?>" onmouseover="playAudio(soundPlayer, 'default.flac');" onclick="window.location.href = '../';">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:84%;position:relative;" src="sys.start.png?rev=<?=time();?>" onclick="window.location.href = 'index.php?mode=menu';">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:84%;position:relative;" src="sys.back.png?rev=<?=time();?>" onclick="window.location.href = 'index.php?mode=view';">
+<img class="hover" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" style="height:84%;position:relative;" src="sys.settings.png?rev=<?=time();?>" onclick="window.location.href = 'index.php?mode=set';">
+<img class="hover" style="height:84%;position:relative;" src="sys.upd.png?rev=<?=time();?>" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" onclick="get('i','','from','windos','','flossely');">
+<img class="hover" style="height:84%;position:relative;" src="sys.exit.png?rev=<?=time();?>" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" onclick="window.location.href = '../';">
 </div>
 <audio id="audioPlayer">
 <audio id="soundPlayer" <?php if (!$sounds) { ?>muted="muted"<?php } ?>>
